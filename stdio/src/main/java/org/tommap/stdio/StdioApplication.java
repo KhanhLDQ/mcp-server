2534e3f2864e29a5ddbd4dc84825ff5732cb9b49
Khanh Le <tommapcoder@gmail.com>
2025-10-27 09:39:27 +0700

    init mcp server via stdio

---
+++ stdio/src/main/java/org/tommap/stdio/StdioApplication.java
@@ -0,0 +1,13 @@
+package org.tommap.stdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StdioApplication.class, args);
+    }
+
+}
---
+++ stdio/src/test/java/org/tommap/stdio/StdioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tommap.stdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
