6a906e513d087bb58c6fe1581316623d3efd4786
Khanh Le <tommapcoder@gmail.com>
2025-10-28 09:20:13 +0700

    init mcp server via streamable http

---
+++ remote/src/test/java/org/tommap/remote/RemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tommap.remote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
